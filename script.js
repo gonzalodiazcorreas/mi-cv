fe3a60db263f4be28fde5816aebc7c0fb58398d9
Gonzalo Díaz Correas <gonzalo.diaz.correas@gmail.com>
2025-12-21 23:39:12 +0100

    Actualización, cargado de archivos desde txt

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
  document.getElementById("anio").textContent = new Date().getFullYear();
 
+
+ function cargarTexto(rutaTxt, idElemento) {
+    fetch(rutaTxt)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("No se pudo cargar " + rutaTxt);
+            }
+            return response.text();
+        })
+        .then(texto => {
+            const elemento = document.getElementById(idElemento);
+            if (elemento) {
+                elemento.textContent = texto;
+            }
+        })
+        .catch(error => {
+            console.error(error);
+        });
+ }
+
+ cargarTexto("txt/_about_me.txt", "text-about_me");
+ 
+ cargarTexto("txt/_profesional.txt", "text-profesional");
+ cargarTexto("txt/_academy.txt", "text-academy");
+
